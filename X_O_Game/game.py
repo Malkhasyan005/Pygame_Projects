92d2a177ecaa5c348d12a0e5ab6e76349449e3c7
Malkhasyan005 <karen.malkhasyan.g@gmail.com>
2021-11-23 01:29:55 +0400

    This is new game. X and O

---
+++ X_O_Game/game.py
@@ -0,0 +1,89 @@
+import random
+try:
+    import numpy as np
+except:
+    print("Please install numpy module")
+
+try:
+    import pygame
+except:
+    print("Please import pygame module")
+from pygame.math import Vector2
+import time
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.width = 800
+        self.height = 800
+        self.bg_color = (28, 170, 156)
+        self.line_color = (23, 145, 135)
+        self.board = np.zeros((3, 3))
+        self.block_size = (800 // 3)
+        self.player = 1
+        self.space = 55
+        self.block_width = 20
+        self.circle_width = 15
+        self.radius = 65
+        self.mouse_pos = None
+        self.surface = pygame.display.set_mode((self.width, self.height))
+        self.running = True
+
+    def draw_x_or_o(self, y, x, player):
+        self.board[y][x] = player
+
+    def empty_block(self, y, x):
+        return self.board[y][x] == 0
+
+    def draw_lines(self):
+        for i in range(1, 3):
+            pygame.draw.line(self.surface, self.line_color, (i * self.width // 3, 0), (i * self.width // 3, 800), 15)
+        for i in range(1, 3):
+            pygame.draw.line(self.surface, self.line_color, (0, i * self.height // 3), (800, i * self.height // 3), 15)
+
+    def draw_blocks(self):
+        for y in range(3):
+            for x in range(3):
+                if self.board[y][x] == 1:
+                    pygame.draw.circle(self.surface, (0, 0, 0), (int(x * self.block_size + self.block_size / 2), int(y * self.block_size + self.block_size / 2)), self.radius, self.circle_width)
+                if self.board[y][x] == 2:
+                    pygame.draw.line(self.surface, (0, 255, 0), (x * self.block_size + self.space, y * self.block_size + self.block_size - self.space),
+                                                            (x * self.block_size + self.block_size - self.space, y * self.block_size + self.space), self.block_width)
+                    pygame.draw.line(self.surface, (0, 255, 0), (x * self.block_size + self.space, y * self.block_size + self.space),
+                                                            (x * self.block_size + self.block_size - self.space, y * self.block_size + self.block_size - self.space), self.block_width)
+
+
+    def update_board(self):
+        if self.empty_block(self.mouse_pos[1], self.mouse_pos[0]):
+            if self.player == 1:
+                self.draw_x_or_o(self.mouse_pos[1], self.mouse_pos[0], self.player)
+                self.player = 2
+            elif self.player == 2:
+                self.draw_x_or_o(self.mouse_pos[1], self.mouse_pos[0], self.player)
+                self.player = 1
+
+
+    def play(self):
+        self.surface.fill(self.bg_color)
+        self.draw_lines()
+        self.draw_blocks()
+
+    def run(self):
+        
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    self.mouse_pos = (int(event.pos[0] / (800 // 3)), int(event.pos[1] / (800 // 3)))
+                    self.update_board()
+
+            self.play()
+
+            pygame.display.update()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
